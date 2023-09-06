cdefa53c8b69ba901bf6b8a6e5e8154b2c54b9fa
tczkqq <jarnutek@gmail.com>
2023-09-06 13:57:46 +0200

    Added 2024 dates

--- main.js
+++ main.js
@@ -5,14 +5,19 @@ const mainMessageContainer = document.querySelector('#main-message');
 
 const currentDate = new Date();
 const shoppingSundays = [
-    new Date(2023, 0, 31).toDateString(),
-    new Date(2023, 2, 28).toDateString(),
-    new Date(2023, 3, 25).toDateString(),
-    new Date(2023, 5, 27).toDateString(),
-    new Date(2023, 7, 29).toDateString(),
-    new Date(2023, 11, 12).toDateString(),
-    new Date(2023, 11, 19).toDateString(),
-
+    new Date(2023, 0, 29).toDateString(),
+    new Date(2023, 3, 2).toDateString(),
+    new Date(2023, 3, 30).toDateString(),
+    new Date(2023, 5, 25).toDateString(),
+    new Date(2023, 7, 27).toDateString(),
+    new Date(2023, 11, 17).toDateString(),
+    new Date(2023, 11, 24).toDateString(),
+    new Date(2024, 0, 28).toDateString(),
+    new Date(2024, 2, 24).toDateString(),
+    new Date(2024, 3, 28).toDateString(),
+    new Date(2024, 7, 25).toDateString(),
+    new Date(2024, 11, 15).toDateString(),
+    new Date(2024, 11, 22).toDateString()
 ]
 currentDateContainer.textContent = `${currentDate.getDate()}.${leadingZero(currentDate.getMonth()+1)}.${currentDate.getFullYear()}r.`
 
