dadf676dc9128277ddbc23f46fa8dc9d430df4b9
Tomeczekqq <jarnutek@gmail.com>
2023-12-30 14:23:55 +0100

    daty 2023

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const mainMessageContainer = document.querySelector('#main-message');
 
 const currentDate = new Date();
 const shoppingSundays = [
-    new Date(2023, 11, 10).toDateString(),
-    new Date(2023, 11, 17).toDateString(),
     new Date(2024, 0, 28).toDateString(),
     new Date(2024, 2, 24).toDateString(),
     new Date(2024, 3, 28).toDateString(),
@@ -14,7 +12,6 @@ const shoppingSundays = [
     new Date(2024, 7, 25).toDateString(),
     new Date(2024, 11, 15).toDateString(),
     new Date(2024, 11, 22).toDateString(),
-    new Date(2025, 0, 26).toDateString(),
 ]
 currentDateContainer.textContent = `${currentDate.getDate()}.${leadingZero(currentDate.getMonth()+1)}.${currentDate.getFullYear()}r.`
 
